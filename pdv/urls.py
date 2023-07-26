9a1741736d0fb56d9b7201093d8ac020597fd3b9
Dhrds <dhrds1996@gmail.com>
2023-07-25 21:56:33 -0300

    .

--- pdv/urls.py
+++ pdv/urls.py
@@ -1,6 +1,14 @@
 from django.urls import path
-from. import views
+from . import views
+
 
 urlpatterns = [
-    path('',views.home,name='home'),
+    path('', views.home, name='home'),
+    path('about/', views.about, name='about'),
+    path('booking/', views.booking, name='booking'),
+    path('contact/', views.contact, name='contact'),
+    path('menu/', views.menu, name='menu'),
+    path('service/', views.service, name='service'),
+    path('team/', views.team, name='team'),
+    path('testimonial/', views.testimonial, name='testimonial'),
 ]
--- pdv/views.py
+++ pdv/views.py
@@ -5,6 +5,49 @@ from .models import Produtos, Categoria
 def home(request):
     categoria = Categoria.objects.all()
     produtos = Produtos.objects.all().select_related('categoria')
-    print(produtos)
     return render(request, 'home.html', {'produtos': produtos,
                                          'categorias': categoria})
+
+
+def about(request):
+    return render(request, 'about.html')
+
+
+def service(request):
+    categoria = Categoria.objects.all()
+    produtos = Produtos.objects.all().select_related('categoria')
+    return render(request, 'service.html', {'produtos': produtos,
+                                            'categorias': categoria})
+
+
+def menu(request):
+    categoria = Categoria.objects.all()
+    produtos = Produtos.objects.all().select_related('categoria')
+    return render(request, 'menu.html', {'produtos': produtos,
+                                         'categorias': categoria})
+
+
+def booking(request):
+
+    return render(request, 'booking.html',)
+
+
+def team(request):
+    categoria = Categoria.objects.all()
+    produtos = Produtos.objects.all().select_related('categoria')
+    return render(request, 'team.html', {'produtos': produtos,
+                                         'categorias': categoria})
+
+
+def testimonial(request):
+    categoria = Categoria.objects.all()
+    produtos = Produtos.objects.all().select_related('categoria')
+    return render(request, 'testimonial.html', {'produtos': produtos,
+                                                'categorias': categoria})
+
+
+def contact(request):
+    categoria = Categoria.objects.all()
+    produtos = Produtos.objects.all().select_related('categoria')
+    return render(request, 'contact.html', {'produtos': produtos,
+                                            'categorias': categoria})
